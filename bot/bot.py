29eb6e20ded0e19090713731ab3dfdbcbf1bc131
mtm-winlhanphyo <win.lhanphyo@seattleconsultingmyanmar.com>
2023-06-26 14:35:00 +0630

    commit for skype message code.

--- bot/bot.py
+++ bot/bot.py
@@ -1,6 +1,68 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask, jsonify, request
 from skpy import Skype
 
-slogin = Skype("email", "password")
+app = Flask(__name__)
 
-contact = slogin.contacts["live:.cid.1ed9002627da4bb3"]
-contact.chat.sendMsg('Testing')
+
+@app.route('/')
+def index():
+    return jsonify("Hello world!")
+
+
+@app.route('/api/skype-message', methods=['POST'])
+def send_skype_message():
+    slogin = Skype("username", "password")
+
+    # Define the table data
+    table_data = [
+        {
+            "label": "Event Name",
+            "value": request.form["event_name"]
+        },
+        {
+            "label": "Description",
+            "value": request.form["description"]
+        },
+        {
+            "label": "Date",
+            "value": request.form["from_date"] + " ~ " + request.form["to_date"]
+        },
+        {
+            "label": "Time",
+            "value": request.form["from_time"] + " ~ " + request.form["to_time"]
+        },
+        {
+            "label": "Address",
+            "value": request.form["address"]
+        },
+        {
+            "label": "Phone Number",
+            "value": request.form["phone"]
+        }
+    ]
+
+    table_str = ''
+    for dic in table_data:
+        for key in dic:
+            table_str += dic[key] + "\n"
+            if key == "value" and table_data[-1][key] != dic[key]:
+                table_str += "\n"
+
+    print(table_str)
+
+    channel = slogin.chats["19:a77d5a5724ce461bb9ca0180bdac1dcf@thread.skype"]
+    channel.sendFile(request.files["image"], "event.jpg", image=True)
+    channel.sendMsg(table_str)
+
+    # to send message to spcific user.
+    # contact = slogin.contacts["live:.cid.47b93bdf06673b7b"]
+    # contact.chat.sendMsg('Testing')
+
+    return jsonify({'name': 'alice',
+                    'email': 'alice@outlook.com'})
+
+
+app.run(debug=True, port=6000)
